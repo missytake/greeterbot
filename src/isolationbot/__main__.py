9a67470e004caaed730c1467ac48bf5f17756219
missytake <missytake@systemli.org>
2022-06-11 13:42:54 +0200

    auto-reply to all incoming messages with error message

--- src/isolationbot/__main__.py
+++ src/isolationbot/__main__.py
@@ -1,3 +1,20 @@
-from . import main
+import deltachat
 
-main()
+
+class AutoReplyPlugin:
+    @deltachat.account_hookimpl
+    def ac_incoming_message(self, message: deltachat.Message):
+        message.create_chat()
+        reply = "Sorry, sending messages to other servers than try.webxdc.org is not " \
+            "supported on this server. If you want to try out Delta Chat, just create" \
+            " another try.webxdc.org account to send messages back and forth. If you " \
+            "have questions, please contact postmaster@try.webxdc.org."
+        message.chat.send_text(reply)
+
+
+def main(argv=None):
+    deltachat.run_cmdline(argv=argv, account_plugins=[AutoReplyPlugin()])
+
+
+if __name__ == "__main__":
+    main()
