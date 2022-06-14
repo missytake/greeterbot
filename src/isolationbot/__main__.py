5d90c46d323468cf9afc83c7c333e053e052f851
missytake <missytake@systemli.org>
2022-06-14 12:02:07 +0200

    ask at the support forum

--- src/isolationbot/__main__.py
+++ src/isolationbot/__main__.py
@@ -9,7 +9,7 @@ class AutoReplyPlugin:
         reply = "Sorry, sending messages to other servers than try.webxdc.org is not " \
             "supported on this server. If you want to try out Delta Chat, just create" \
             " another try.webxdc.org account to send messages back and forth. If you " \
-            "have questions, please contact postmaster@try.webxdc.org."
+            "have questions, please ask around at https://support.delta.chat."
         message.chat.send_text(reply)
 
 
