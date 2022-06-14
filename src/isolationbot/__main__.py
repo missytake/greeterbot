fdc49da429dc31aa206ed60464203207260f12bd
missytake <missytake@systemli.org>
2022-06-14 12:22:46 +0200

    make a reply out of the message

--- src/isolationbot/__main__.py
+++ src/isolationbot/__main__.py
@@ -6,11 +6,14 @@ class AutoReplyPlugin:
     def ac_incoming_message(self, message: deltachat.Message):
         message.account.set_avatar("assets/avatar.jpg")
         message.create_chat()
-        reply = "Sorry, sending messages to other servers than try.webxdc.org is not " \
+        replytext = "Sorry, sending messages to other servers than try.webxdc.org is not " \
             "supported on this server. If you want to try out Delta Chat, just create" \
             " another try.webxdc.org account to send messages back and forth. If you " \
             "have questions, please ask around at https://support.delta.chat."
-        message.chat.send_text(reply)
+        reply = deltachat.message.Message.new_empty(message.account, "text")
+        reply.quote = message
+        reply.set_text(replytext)
+        message.chat.send_msg(reply)
 
 
 def main(argv=None):
